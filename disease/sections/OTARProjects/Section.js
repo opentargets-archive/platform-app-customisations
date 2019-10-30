9d515bce324e25ee4afc861c32144ce0e8befc56
Alfredo Miranda <alfredo@miranda.io>
2019-10-30 12:14:21 +0000

    Add projects table

--- disease/sections/OTARProjects/Section.js
+++ disease/sections/OTARProjects/Section.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import projects from './projects.json';
+import { OtTableRF } from 'ot-ui';
+
+const columns = [
+  {
+    id: 'projectId',
+    label: 'Project ID',
+  },
+  {
+    id: 'name',
+    label: 'Name'
+  },
+  {
+    id: 'efoId',
+    label: 'EFO ID',
+  }
+]
 
 const Section = () => {
-  return <div>OTAR projects</div>;
+  return <OtTableRF columns={columns} data={projects}/>
 };
 
 export default Section;
