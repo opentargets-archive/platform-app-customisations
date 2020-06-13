b010db16c43ca4fdda702a31b8d3cfd0ce6b9980
Alfredo Miranda <alfredo@miranda.io>
2020-06-12 21:35:51 -0500

    Update BasePage

--- common/BasePage.js
+++ common/BasePage.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { makeStyles } from '@material-ui/core/styles';
 import { Page, NavBar } from 'ot-ui';
+
+import { externalLinks, mainMenuItems } from '../../constants';
+import Search from './search/Search';
 import CustomFooter from './CustomFooter';
 
-const BasePage = ({ children }) => (
-  <Page
-    header={<NavBar name="Platform" />}
-    footer={<CustomFooter/>}
-  >
-    <Helmet
-      defaultTitle="Open Targets Platform"
-      titleTemplate="%s | Open Targets Platform"
-    />
-    {children}
-  </Page>
-);
+const useStyles = makeStyles({
+  searchContainer: {
+    minWidth: '400px',
+  },
+});
+
+const BasePage = ({ children }) => {
+  const classes = useStyles();
+  return (
+    <Page
+      header={
+        <NavBar
+          name="Platform"
+          search={
+            <div className={classes.searchContainer}>
+              <Search embedded />
+            </div>
+          }
+          items={mainMenuItems}
+        />
+      }
+      footer={<CustomFooter externalLinks={externalLinks} />}
+    >
+      <Helmet
+        defaultTitle="Open Targets Platform"
+        titleTemplate="%s | Open Targets Platform"
+      />
+      {children}
+    </Page>
+  );
+};
 
 export default BasePage;
